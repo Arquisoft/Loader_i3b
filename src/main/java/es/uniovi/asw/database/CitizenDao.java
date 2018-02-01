7c87c8b1c502d52e4b9a42a46c4b564a2e5d9725
sergiosantano <sergiosantano96@gmail.com>
2018-02-01 19:06:25 +0100

    prueba desde eclipse

--- src/main/java/es/uniovi/asw/database/CitizenDao.java
+++ src/main/java/es/uniovi/asw/database/CitizenDao.java
@@ -7,7 +7,7 @@ import es.uniovi.asw.parser.Citizen;
 /**
  * Interface for the methods that the DAO must accomplish
  * 
- * @author Gonzalo de la Cruz Fernández - UO244583 - Prueba
+ * @author Gonzalo de la Cruz Fernández - UO244583
  *
  */
 public interface CitizenDao {
